67ed6aa54ae1d7126e3202909fcea1b8dbfe56d5
ArnaudLouisy <louisy.arnaud@gmail.com>
2022-03-16 15:56:32 +0100

    Fix : erreur de coups

--- src/main/java/com/example/demomorpion/Morpion.java
+++ src/main/java/com/example/demomorpion/Morpion.java
@@ -1,9 +1,11 @@
 package com.example.demomorpion;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,6 +19,8 @@ public class Morpion implements Initializable {
 
     @FXML
     private ListView<String> listeCoups;
+    @FXML
+    private GridPane maGrid;
 
     public Morpion(String joueurun, String joueurdeux) {
         Joueurun = joueurun;
@@ -27,9 +31,8 @@ public class Morpion implements Initializable {
     @FXML
     void OnClickLabel(MouseEvent event) {
         Label label = (Label) event.getSource();
+        if (lblJoueur.getText().isBlank())
         label.setText(symbol);
-        if(label.getText().isEmpty());{
-
 
         if (symbol.equals(("X"))) {
             symbol = "O";
@@ -40,11 +43,13 @@ public class Morpion implements Initializable {
             this.lblJoueur.setText(this.Joueurun);
             listeCoups.getItems().add(Joueurdeux+"a fais un coup");
         }
-        }else{
+    }
 
-        }
 
-        }
+
+
+
+
 
 
 
@@ -52,4 +57,18 @@ public class Morpion implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         lblJoueur.setText(Joueurun);
     }
+    private Label getNodeFromGrindidPane(int col, int row){
+        for (Node node : maGrid.getChildren()){
+            if(GridPane.getColumnIndex(node)==col && GridPane.getRowIndex(node)==row){
+                return(Label) node;
+            }
+        }
+
+
+        return null;
+    }
 }
+
+
+
+
